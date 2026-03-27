156edcdbecdef3af11b566419fec7d2faada47f8
Raiyan Khan <85121104+Raiyankhan640@users.noreply.github.com>
2026-03-27 21:18:46 +0600

    web structure created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='my-app-container'>
+        <h1 className='text-2xl text-center'>Automated Tender Intelligence System</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
